d191951312e0f8c9950984f629fb303493acf38f
Marvellousz <pranavmurali024@gmail.com>
2024-12-06 19:10:42 +0000

    fixed workout details

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,38 +1,40 @@
-import { Link } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 import { useAuthContext } from '../hooks/useAuthContext'
 
-const Navbar = () => {
-  const { logout } = useLogout()
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext()
   const { user } = useAuthContext()
 
-  const handleClick = () => {
-    logout()
+  const handleClick = async () => {
+    if (!user) {
+      return
+    }
+
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
   }
 
   return (
-    <header>
-      <div className="container">
-        <Link to="/">
-          <h1>FitTrack</h1>
-        </Link>
-        <nav>
-          {user && (
-            <div>
-              <span>{user.email}</span>
-              <button onClick={handleClick}>Log out</button>
-            </div>
-          )}
-          {!user && (
-            <div>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </div>
-          )}
-        </nav>
-      </div>
-    </header>
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p><strong>Load (kg): </strong>{workout.load}</p>
+      <p><strong>Reps: </strong>{workout.reps}</p>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+    </div>
   )
 }
 
-export default Navbar
+export default WorkoutDetails
